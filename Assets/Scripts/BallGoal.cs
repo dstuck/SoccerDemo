6d16d1159fcced258c278ad8038ed70c1d37e62b
dstuck <david.e.stuck@gmail.com>
2021-02-27 15:52:15 -0600

    Hack around webgl input issue

--- Assets/Scripts/BallGoal.cs
+++ Assets/Scripts/BallGoal.cs
@@ -13,6 +13,7 @@ public class BallGoal : MonoBehaviour
     public Vector2 targetPosition { get { return _movementGoal + ballRigidbody2d.position; } }
     TeamControls controls;
     PlayerInput playerInput;
+    private Keyboard kb; //hack around webgl input incompatability
 
     float kickFactor = 4.0f;
 
@@ -21,13 +22,14 @@ public class BallGoal : MonoBehaviour
     {
         ballRigidbody2d = GameObject.FindWithTag("Ball").GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
+        kb = Keyboard.current;
         InputUser.PerformPairingWithDevice(Keyboard.current, user: playerInput.user);
         playerInput.user.ActivateControlScheme(playerInput.defaultControlScheme);
     }
 
     public void OnEnable()
     {
-        if (controls == null)
+        if (kb == null)
         {
             controls = new TeamControls();
             // Tell the "gameplay" action map that we want to get told about
@@ -46,20 +48,27 @@ public class BallGoal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _movementGoal = playerInput.actions["Movement"].ReadValue<Vector2>();
-
-        float distanceFactor = baseDistanceGoal;
-
-        if (playerInput.actions["Kick"].ReadValue<float>() > 0.0f)
+        if (kb != null)
         {
-            distanceFactor *= kickFactor;
-        }
+            _movementGoal = playerInput.actions["Movement"].ReadValue<Vector2>();
 
-        // If we're using a square input, force it back into a unit circle
-        if (_movementGoal.SqrMagnitude() > 1)
-        {
-            _movementGoal.Normalize();
+            float distanceFactor = baseDistanceGoal;
+
+            if (playerInput.actions["Kick"].ReadValue<float>() > 0.0f)
+            {
+                distanceFactor *= kickFactor;
+            }
+
+            // If we're using a square input, force it back into a unit circle
+            if (_movementGoal.SqrMagnitude() > 1)
+            {
+                _movementGoal.Normalize();
+            }
+            _movementGoal *= distanceFactor;
         }
-        _movementGoal *= distanceFactor;
+        else
+        {
+            kb = Keyboard.current; // as a workaround: keep assigning the device value until it's not null
+        }
     }
 }
